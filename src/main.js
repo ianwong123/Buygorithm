af3b93bd78a052c38634c4adc613b4f1eb3d1133
projectuser1 <ianwong2111@gmail.com>
2025-03-04 20:30:33 +0800

    Initial project setup

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// This file is the entry point of the app. It creates a Vue instance
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './assets/tailwind.css';
+
+createApp(App).mount('#app')
