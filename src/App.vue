6d0564e1e139191eac51f6c3f2ea6087925cbc4a
projectuser1 <ianwong2111@gmail.com>
2025-03-05 06:05:31 +0800

    added 'views' directory for different pages of app

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,13 @@
 <!-- This file is used by main.js to create the app -->
 <!-- javascript logic, data handling-->
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
 </script>
 
 <!-- html based code that is rendered to the screen-->
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="nonono" />
+
 </template>
 
 <!-- css styles for components-->
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
 </style>
