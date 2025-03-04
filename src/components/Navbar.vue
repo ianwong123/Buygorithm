059946f5a8769aba96783138825fdbc3af1903a4
projectuser1 <ianwong2111@gmail.com>
2025-03-05 06:17:39 +0800

    modified navbar and added new router file

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="flex justify-between items-center p-4 bg-blue-500 text-white">
+      <div class="text-xl font-bold">My E-Commerce</div>
+      <div class="flex space-x-4">
+        <router-link to="/cart" class="hover:text-gray-200">Cart</router-link>
+        <router-link to="/login" class="hover:text-gray-200">Login</router-link>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  };
+  </script>
+  
+  <style scoped>
+  </style>
