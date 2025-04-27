0715aeef3e1389cb6d78ac48d625a57b8a88b274
Gronola <granariobrent123@gmail.com>
2025-04-27 14:41:00 +0100

    Loyalty Program

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,6 +10,7 @@
       <router-link to="/trending" class="hover:text-gray-200">Shop</router-link>
       <router-link to="/sell" class="hover:text-gray-200">Sell</router-link>
       <router-link to="/manage" class="hover:text-gray-200">Manage Products</router-link>
+      <router-link to="/loyalty" class="hover:text-gray-200">Loyalty</router-link>
       <router-link to="/cart" class="hover:text-gray-200">Cart</router-link>
       <button @click="openLoginModal" class="hover:text-gray-200">Login</button>
       <button @click="openSignupModal" class="hover:text-gray-200">Sign Up</button>
@@ -34,6 +35,7 @@
       <router-link to="/trending" class="text-2xl my-2 hover:text-gray-200" @click="toggleMenu">Shop</router-link>
       <router-link to="/sell" class="text-2xl my-2 hover:text-gray-200" @click="toggleMenu">Sell</router-link>
       <router-link to="/manage" class="text-2xl my-2 hover:text-gray-200" @click="toggleMenu">Manage Products</router-link>
+      <router-link to="/loyalty" class="text-2xl my-2 hover:text-gray-200" @click="toggleMenu">Loyalty</router-link>
       <router-link to="/cart" class="text-2xl my-2 hover:text-gray-200" @click="toggleMenu">Cart</router-link>
       <button @click="openLoginModal" class="text-2xl my-2 hover:text-gray-200">Login</button>
       <button @click="openSignupModal" class="text-2xl my-2 hover:text-gray-200">Sign Up</button>
